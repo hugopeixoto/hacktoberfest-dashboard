326cb8cb8d73716121653cb254f4c5485265c115
Hugo Peixoto <hugo.peixoto@gmail.com>
2020-10-01 22:50:46 +0100

    Strip whitespace from github usernames

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,17 @@ class User < ApplicationRecord
   scope :active, -> { where(active: true) }
   has_one :user_statistic
 
+  before_save :strip_username_whitespace
+
   def github_avatar_url
     "https://github.com/#{github_username}.png"
   end
 
   def github_url
     "https://github.com/#{github_username}"
+  end
+
+  def strip_username_whitespace
+    self.github_username.strip!
   end
 end
